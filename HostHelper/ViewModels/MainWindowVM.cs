58b61a87b13a1aeec23443d5ae20a652aaeab5b7
sidsoft2014 <sidsoft2014@outlook.com>
2016-06-12 10:46:31 +0100

    initial commit

---
+++ HostHelper/ViewModels/MainWindowVM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostHelper.ViewModels
+{
+    class MainWindowVM : ViewModelBase
+    {
+        private string[] _hostFileText = new[] { "Here is hosts text", "Here is hosts text again", "And again here is hosts text again" };
+        private string[] _vHosts = new[] { "V Host 1", "V host 2", "www.hdsjdhaskjd.ddhsajdak.sadjhaskdhaskdj.uk.net.com" };
+
+        public MainWindowVM()
+        {
+        }
+
+        public string Name
+        {
+            get { return "Some name"; }
+        }
+        public string[] HostFileText
+        {
+            get
+            {
+                return _hostFileText;
+            }
+            private set
+            {
+                if (_hostFileText != value)
+                {
+                    _hostFileText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string[] VirtualHosts
+        {
+            get
+            {
+                return _vHosts;
+            }
+            private set
+            {
+                if (_vHosts != value)
+                {
+                    _vHosts = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
---
+++ HostHelper/ViewModels/ViewModelBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostHelper.ViewModels
+{
+    abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    PropertyChanged = null;
+                }
+
+                disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
