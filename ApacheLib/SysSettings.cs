b77ba69858a1896969eee8427d1bf4b4bb95d3ce
sidsoft2014 <sidsoft2014@outlook.com>
2016-06-13 22:12:24 +0100

    Final commit for day. Minor tweaks

--- ApacheLib/SysSettings.cs
+++ ApacheLib/SysSettings.cs
@@ -4,14 +4,22 @@ using System;
 
 namespace ApacheLib
 {
+    /// <summary>
+    /// Sets the system specific services and initialises the library.
+    /// This should be run before any other classes are initialised.
+    /// </summary>
     public static class SysSettings
     {
-        public static IAppSettings AppSettings { get; private set; }
-        public static IFileService FileService { get; private set; }
-
+        internal static IAppSettings AppSettings { get; private set; }
+        internal static IFileService FileService { get; private set; }
         internal static VirtualHostService VHS;
         internal static HostFileService HFS;
 
+        /// <summary>
+        /// Set system specific services and initialise the library.
+        /// </summary>
+        /// <param name="fileService">The system specfic implementation of the IFileService</param>
+        /// <param name="appSettings">The system specfic implementation of IAppSettings</param>
         public static void Init(IFileService fileService, IAppSettings appSettings)
         {
             if (fileService == null)
