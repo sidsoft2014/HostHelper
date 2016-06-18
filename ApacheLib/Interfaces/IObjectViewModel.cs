0ae430ff406f6dd5bf20857a841408ec8c8045c4
sidsoft2014 <sidsoft2014@outlook.com>
2016-06-18 21:39:59 +0100

    Added missed file

---
+++ ApacheLib/Interfaces/IObjectViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApacheLib.Interfaces
+{
+    public interface IObjectViewModel
+    {
+        void SetSelectedObject(object obj);
+        void Save();
+
+        event EventHandler OnSaved;
+    }
+}
